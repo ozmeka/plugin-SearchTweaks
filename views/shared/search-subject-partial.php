bc4f01c76bf87f12025387fa98465a1fbc55476e
Thom Mc <thomas.mcintyre@uts.edu.au>
2015-07-28 05:39:24 +0000

    Fix breaking search a la https://github.com/ozmeka/plugin-SearchTweaks/issues/1

--- views/shared/search-subject-partial.php
+++ views/shared/search-subject-partial.php
@@ -4,7 +4,7 @@
             </div>
             <div class="five columns omega inputs">
             <?php
-                echo $this->formHidden('advanced[999][element_id]', $this->subject_element_id);
+                echo $this->formHidden('advanced[999][element_id]', null);
                 echo $this->formHidden('advanced[999][type]', $this->subject_match_type);
                 echo $this->formSelect(
                     'advanced[999][terms]',
@@ -12,6 +12,20 @@
                     array('id' => 'item-subject-search'),
                     $this->subjects
                 );
+                
+                //  javascript to prevent advanced search allowing an empty terms string 
+                //  through which it does when element_id has a value...
             ?>
+                <script language="javascript" type="text/javascript">
+                    jQuery('#item-subject-search').change(function () {
+                        if (jQuery('#item-subject-search').val().trim()) {
+                            jQuery('#advanced-999-element_id').val(<?php echo $this->subject_element_id; ?>);
+                        }
+                        else
+                        {
+                            jQuery('#advanced-999-element_id').val(null);
+                        }
+                    });
+                </script>
             </div>
         </div>
